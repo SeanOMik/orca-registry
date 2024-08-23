1e5f0e7daf819e6e9d691689b9c41e029ff54941
SeanOMik <seanomik@gmail.com>
2024-08-22 21:09:09 -0400

    Move some code around

--- crates/orca_api/src/api/mod.rs
+++ crates/orca_api/src/api/mod.rs
@@ -1,25 +1,2 @@
-use std::sync::Arc;
-
-use axum::extract::State;
-use axum::response::{IntoResponse, Response};
-use axum::http::{StatusCode, HeaderName};
-
-use crate::app_state::AppState;
-
-pub mod blobs;
-pub mod uploads;
-pub mod manifests;
-pub mod tags;
-pub mod catalog;
-pub mod auth;
-
-/// https://docs.docker.com/registry/spec/api/#api-version-check
-/// full endpoint: `/v2/`
-pub async fn version_check(_state: State<Arc<AppState>>) -> Response {
-    (
-        StatusCode::OK,
-        [
-            ( HeaderName::from_static("docker-distribution-api-version"), "registry/2.0" ),
-        ]
-    ).into_response()
-}
+pub mod oci;
+pub mod orca;
