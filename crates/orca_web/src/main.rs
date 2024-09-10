c30ac0872c18f4d9abbee7d80d0134eacad9910d
SeanOMik <seanomik@gmail.com>
2024-09-09 20:27:44 -0400

    Switch to leptos for the frontend

--- crates/orca_web/src/main.rs
+++ crates/orca_web/src/main.rs
@@ -1,24 +1,50 @@
-use yew::prelude::*;
+use leptos::{component, create_signal, view, CollectView, IntoView, ReadSignal, Signal, SignalUpdate};
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
+#[component]
+fn ProgressBar(
+    #[prop(default = 100)]
+    max: u16,
+    // this isn't a ReadSignal<i32> so that `double_count` can also be accepted.
+    //progress: impl Fn() -> i32 + 'static,
+    #[prop(into)]
+    progress: Signal<i32>
+) -> impl IntoView {
+    view! {
+        <progress
+            max=max
+            value=progress
+        />
+        <br/>
+    }
+}
 
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+#[component]
+fn App() -> impl IntoView {
+    // create a list of 5 signals
+    let length = 5;
+    let counters = (1..=length).map(|idx| create_signal(idx));
+
+    // each item manages a reactive view
+    // but the list itself will never change
+    let counter_buttons = counters
+        .map(|(count, set_count)| {
+            view! {
+                <li>
+                    <button
+                        on:click=move |_| set_count.update(|n| *n += 1)
+                    >
+                        {count}
+                    </button>
+                </li>
+            }
+        })
+        .collect_view();
+
+    view! {
+        <ul>{counter_buttons}</ul>
     }
 }
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    leptos::mount_to_body(|| view! { <App/> })
 }
